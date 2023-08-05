c510cc4963d9f548e3bad9fd80f51d950ea20ed4
Autumn Han <autumn9894@gmail.com>
2023-08-05 14:34:46 -0700

    registration validations working on front and backend

--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const RegistrationForm = () => {
-    // const [ errors, setErrors ] = useState([]);
+    const [ errors, setErrors ] = useState([]);
     const [ userName, setUsername ] = useState("");
     const [ passWord, setPassword ] = useState("");
     const [ confirmPassword, setConfirmPassword ] = useState("");
@@ -17,12 +17,23 @@ const RegistrationForm = () => {
             })
             .catch((err) => {
                 console.log("Unable to process POST registration request");
+                const errorResponse = err.response.data.errors;
+                const errorArr = [];
+                for (const key of Object.keys(errorResponse)) {
+                    errorArr.push(errorResponse[key].message)
+                }
+                setErrors(errorArr);
             });
     };
     return (
         <div>
             <h2>Ready to Start?</h2>
             <form onSubmit={submitHandler}>
+                <div>
+                    {errors.map((err, index) => (
+                        <p key={index}>{err}</p>
+                    ))}
+                </div>
                 <div>
                     <label htmlFor="userName">Username: </label>
                     <input type="text" name="userName" value={userName} onChange={(e) => setUsername(e.target.value)} />
