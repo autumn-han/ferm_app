1185250a8f19fbe1ec4dade4cf84a2c9a86ce978
Autumn Han <autumn9894@gmail.com>
2023-08-05 11:33:22 -0700

    added authenticate to getAllUsers and getUser

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -1,9 +1,10 @@
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 
+// creating a method that checks user authentication
 module.exports = {
   authenticate(req, res, next) {
-    jwt.verify(req.cookies.adminToken, secret, (err, payload) => {
+    jwt.verify(req.cookies.userToken, secret, (err, payload) => {
       if (err) {
         res.status(401).json({ verified: false });
       } else {
