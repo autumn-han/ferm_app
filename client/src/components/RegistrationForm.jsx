76d2bcce921c214117864eef60346b455810102d
Autumn Han <autumn9894@gmail.com>
2023-08-05 15:12:31 -0700

    modularized FoyerPage

--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
 
-const RegistrationForm = () => {
-    const [ errors, setErrors ] = useState([]);
+const RegistrationForm = (props) => {
+    const { onSubmitProp, errors } = props;
+    // const [ errors, setErrors ] = useState([]);
     const [ userName, setUsername ] = useState("");
     const [ passWord, setPassword ] = useState("");
     const [ confirmPassword, setConfirmPassword ] = useState("");
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const submitHandler = (e) => {
         e.preventDefault();
-        axios.post('http://localhost:8000/api/user/register', { userName, passWord, confirmPassword }, { withCredentials: true })
-            .then((newUser) => {
-                console.log(newUser);
-                navigate('/');
-            })
-            .catch((err) => {
-                console.log("Unable to process POST registration request");
-                const errorResponse = err.response.data.errors;
-                const errorArr = [];
-                for (const key of Object.keys(errorResponse)) {
-                    errorArr.push(errorResponse[key].message)
-                }
-                setErrors(errorArr);
-            });
+        onSubmitProp({ userName, passWord, confirmPassword });
+        // axios.post('http://localhost:8000/api/user/register', { userName, passWord, confirmPassword }, { withCredentials: true })
+        //     .then((newUser) => {
+        //         console.log(newUser);
+        //         navigate('/');
+        //     })
+        //     .catch((err) => {
+        //         console.log("Unable to process POST registration request");
+        //         const errorResponse = err.response.data.errors;
+        //         const errorArr = [];
+        //         for (const key of Object.keys(errorResponse)) {
+        //             errorArr.push(errorResponse[key].message)
+        //         }
+        //         setErrors(errorArr);
+        //     });
     };
     return (
         <div>
--- client/src/views/FoyerPage.jsx
+++ client/src/views/FoyerPage.jsx
@@ -1,8 +1,28 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 import RegistrationForm from '../components/RegistrationForm';
 
 const FoyerPage = () => {
+    const [ errors, setErrors ] = useState([]);
+    const navigate = useNavigate();
+    const registerUser = (userParam) => {
+        axios.post('http://localhost:8000/api/user/register', userParam, { withCredentials: true })
+            .then((newUser) => {
+                console.log(newUser);
+                navigate('/');
+            })
+            .catch((err) => {
+                console.log("Unable to process POST registration request");
+                const errorResponse = err.response.data.errors;
+                const errorArr = [];
+                for (const key of Object.keys(errorResponse)) {
+                    errorArr.push(errorResponse[key].message)
+                }
+                setErrors(errorArr);
+            });
+    }
     return (
         <div>
             <div>
@@ -13,7 +33,7 @@ const FoyerPage = () => {
                     <LoginForm />
                 </div>
                 <div>
-                    <RegistrationForm />
+                    <RegistrationForm errors={errors} onSubmitProp={registerUser} />
                 </div>
             </div>
         </div>
