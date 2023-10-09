18097b9f34b3b2b3874ddef98ddff24be1298c93
Autumn Han <autumn9894@gmail.com>
2023-10-09 11:17:14 -0700

    installed multer@1.4.4 and multer-gridfs-storage for large file storage in db

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const { GridFSStorage } = require("multer-gridfs-storage");
 
 require("./config/mongoose.config");
 require("dotenv").config();
