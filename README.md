85399c414f21c99dde3bbf5cf7fbc1e7c2eaa8c9
Autumn Han <autumn9894@gmail.com>
2023-10-24 15:57:02 -0700

    edited dependencies in server.js / built out gridFS config and storage in server.js

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ Project pages will capture information about the fermentation process as well as
  - fix authenticate for delete methods
 
 # RECENT ACTIVITY
-- added body-parser (assist with data transformation from forms), ejs, and method-override (assist with deleting without an AJAX call) as packages 
 - added encType attribute to log entry form on ProjectDetail component
 - imported dependencies into server file
 - added 'upload' to User model file (type: String), within logEntries
 - added middleware into server file
+- rearranged server.js file
 
 # NOTES
 - when testing routes via Postman, remove 'authenticate' parameter to allow server access
--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 const multer = require("multer");
-const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const { GridFsStorage } = require("multer-gridfs-storage");
 const methodOverride = require("method-override");
-
-// gridFS middleware
-app.use(bodyParser.json());
-app.use(methodOverride("_method"));
+const crypto = require("crypto");
+const path = require("path");
 
 require("./config/mongoose.config");
 require("dotenv").config();
@@ -18,6 +18,42 @@ app.use(cookieParser());
 app.use(cors({ origin: process.env.DB_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// gridFS middleware
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+
+// create connection
+const conn = mongoose.createConnection(process.env.MongoDBURL);
+
+// initialize gfs
+let gfs;
+
+// initialize stream
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+// create storage
+const storage = new GridFsStorage({
+  url: process.env.MongoDBURL,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucket: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+const upload = multer({ storage });
 
 const userRoutes = require("./routes/users.routes");
 userRoutes(app);
